97c9ad62d91654caba732cff2138f3548845c979
lisitsinyr <lisitsinyr@gmail.com>
2023-12-11 19:08:41 +0400

    11.12.2023

--- CreateGIT.bat
+++
@@ -1,7 +0,0 @@
-echo "# EXAMPLES_PY" >> README.md
-git init
-git add README.md
-git commit -m "BEGIN"
-git branch -M main
-git remote add origin https://github.com/lisitsinyr/EXAMPLES_PY.git
-git push -u origin main
